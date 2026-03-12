027ec602bfbbe63e2a864d675340d404e92a7376
Garvit Singh Rathore <garvit.sr7@gmail.com>
2026-03-12 15:28:20 +0530

    Main file

--- model/model.py
+++ model/model.py
@@ -2,7 +2,7 @@ from nilearn import plotting
 import matplotlib.pyplot as plt
 
 # Update this path to exactly match where your new .nii.gz file is located
-fmri_file_path = r"F:\Dyslexia\model\datasets\ds003126-download\sub-047EPKL014067\ses-2\func\sub-047EPKL014067_ses-2_task-read_run-01_bold.nii.gz"
+fmri_file_path = r"F:\Dyslexia\model\datasets\fmri\ds003126\sub-047EPKL014067\ses-2\func\sub-047EPKL014067_ses-2_task-read_run-01_bold.nii.gz"
 
 # This will plot the structural brain image 
 print("Loading fMRI data...")
